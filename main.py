033d5d2a31eca0954a384835a11b9c1064be0ae0
Hants Williams <hantsawilliams@gmail.com>
2023-09-23 22:45:20 -0400

    added in file

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template_string, request
+import pandas as pd
 
 from dashboard_builder import get_dashboard_template  # noqa: E402
 from dashboard_builder.config import Config # noqa: E402
 from dashboard_builder.components.inputs import InputDropdown, InputSlider_Categorical, InputRadio # noqa: E402, E501
-from dashboard_builder.components.outputs import OutputMarkdown # noqa: E501, E402
+from dashboard_builder.components.outputs import OutputText, OutputChart_Matplotlib, OutputTable_HTML, OutputImage, OutputMarkdown # noqa: E501, E402
 from dashboard_builder.components.managers import ComponentManager, FormGroup # noqa: E402, E501
 
 app = Flask(__name__)
+
+dashboard_settings = Config(
+    footer_text="Built by Hants Williams, PhD, RN - Clinical Assistant Professor - Stony Brook University, School of Health Professions - Applied Health Informatics" # noqa: E501
+    )
+
+df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/dashboard-builder/main/example_dashboards/app2/ny_suffolk_nassau.csv')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,14 +21,36 @@ def index():
     # Step 1: Initialize the component manager for this request/endpoint
     manager = ComponentManager(request)
 
+    # Step 1: Initialize the component manager for this request/endpoint
+    manager = ComponentManager(request)
+
     # Step 2: Registering and capturing inputs for this request
-    basic_group = FormGroup(action_url='/', markdown_top="""### Basic Inputs""", markdown_bottom="""*Test Markdown.*""")
-    input1_dropdown = InputDropdown(name='dropdown1', label='Select a value:', values=['A', 'B', 'C'])
-    input2_slider = InputSlider_Categorical(name='slider1', label='Select a value:', categories=['A', 'B', 'C'])
-    input3_radio = InputRadio(name='radio1', label='Select a value:', options=['A', 'B', 'C'])
-    basic_group.add_inputs(input1_dropdown, input2_slider, input3_radio)
-    manager.register_inputs(input1_dropdown, input2_slider, input3_radio)
-    manager.register_form_groups(basic_group)
+    # We can separate these into distinct groups, so here is the first group: 
+    # In this below example, we are adding/registering multiple inputs to a group 
+    hospital_form_group = FormGroup(action_url='/', markdown_top="""### Select a Specific Hospital""", markdown_bottom="""*Use this section to filter by a specific hospital.*""") # noqa: E501
+    input2_dropdown = InputDropdown(name='hospital_selection', label='Select a hospital:', values=(df, 'Hospital Name')) # noqa: E501
+    hospital_form_group.add_inputs(input2_dropdown)
+    manager.register_inputs(input2_dropdown)
+    manager.register_form_groups(hospital_form_group)
+
+    # Step 2b: Lets now create a second group of inputs
+    # This one will have a dropdown based on the Number of Beds,
+    #  where we have a list of <100, or >100
+    # In this examle below, we are just registering a single input to a group
+    hospital_form_group2 = FormGroup(action_url='/', markdown_top="""### Group by Hospital Bed Count""", markdown_bottom="""*Use this section to filter by a number of beds.*""") # noqa: E501
+    input2_slider = InputSlider_Categorical(name='bed_selection', label='Select a number of beds:', categories=['Select All', 'hospitals < 100 beds', '100 beds >= hospitals < 300 beds', 'hospitals >= 300 beds']) # noqa: E501
+    hospital_form_group2.add_inputs(input2_slider)
+    manager.register_inputs(input2_slider)
+    manager.register_form_groups(hospital_form_group2)
+
+    # Step 2c: Lets create a dropdown that can filter if the net 
+    # income is positive or negative. In this examle below, we are just registering 
+    # a single input to a group
+    hospital_form_group3 = FormGroup(action_url='/', markdown_top="""### Group by Net Income""", markdown_bottom="""*Use this section to filter by a net income.*""") # noqa: E501
+    input2_radio = InputRadio(name='net_income_selection', label='Select a net income:', options=['Positive', 'Negative']) # noqa: E501
+    hospital_form_group3.add_inputs(input2_radio)
+    manager.register_inputs(input2_radio)
+    manager.register_form_groups(hospital_form_group3)
 
     # Step 3: Do the normal python processing stuff of your data:
 
